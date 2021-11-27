23c689e9e950abfa8e666369a8b6855e65e9caa3
salamander <salamander@example.com>
2021-11-27 15:41:43 +0600

    AopLoggingHandler add object mapper as post construct

--- app/src/main/java/com/sme/todo/advice/AopLoggingHandler.java
+++ app/src/main/java/com/sme/todo/advice/AopLoggingHandler.java
@@ -1,6 +1,7 @@
 package com.sme.todo.advice;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.extern.slf4j.Slf4j;
@@ -9,18 +10,25 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @Aspect
 @Component
 @Slf4j
 public class AopLoggingHandler {
 
-    @Around("execution(* com.sme.todo.*.*.*(..))")
-    public Object logger(ProceedingJoinPoint joinPoint) throws Throwable {
-        ObjectMapper mapper = new ObjectMapper();
+    private ObjectMapper mapper;
+
+    @PostConstruct
+    private void postContruct() {
+        mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
         mapper.registerModule(new Jdk8Module());
-        mapper.configure(com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+    }
 
+    @Around("execution(* com.sme.todo.*.*.*(..))")
+    public Object logger(ProceedingJoinPoint joinPoint) throws Throwable {
         String methodName = joinPoint.getSignature().getName();
         String className = joinPoint.getTarget().getClass().toString();
 
